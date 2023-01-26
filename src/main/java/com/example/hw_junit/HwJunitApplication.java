f6af78466f053a56a7f5e985bc63a5af5a43f119
Evgenia Ganich <evgeniya.bw@gmail.com>
2023-01-26 18:16:09 +0300

    Initial commit

---
+++ src/main/java/com/example/hw_junit/HwJunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw_junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hw_junit/HwJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
